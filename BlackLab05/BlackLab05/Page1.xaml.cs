8f177c8e966b02846f24895880b26e751d1bc16a
Alumno <Alumno@LAB1509-04>
2023-04-28 17:08:55 -0500

    Agregar archivos de proyecto.

---
+++ BlackLab05/BlackLab05/Page1.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BlackLab05
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            StackLayout stackLayout = new StackLayout();
+
+            Label label = new Label();
+
+            label.Text = "Black";
+            label.BackgroundColor= Color.Blue;
+
+
+            
+            stackLayout.Children.Add(label);
+
+            for(int i = 0;i < 5; i++)
+            {
+                Button button = new Button();
+                button.Text = "Eduardo";
+                button.BackgroundColor = Color.Green;
+                stackLayout.Children.Add(button);
+            }
+
+
+            this.Content= stackLayout;
+        }
+    }
+}
